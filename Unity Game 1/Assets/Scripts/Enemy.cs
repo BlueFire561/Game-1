f0e297247fafc4dac51f1817a94f3dac9cc5852d
Jack Stanbridge <supbscripter@gmail.com>
2017-12-21 20:36:21 +0000

    Created Enemy Class. Enemy now moves towards player.

---
+++ Unity Game 1/Assets/Scripts/Enemy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public int Speed;
+    public GameObject Player;
+    private Rigidbody2D rigidbody2D;
+	// Use this for initialization
+	void Start () {
+        rigidbody2D = this.GetComponent<Rigidbody2D>();
+	}
+	
+	// FixedUpdate is called once per fixeddeltatime
+	void FixedUpdate () {
+
+        // Rotate Towards Player
+        Vector3 diff = Player.transform.position - transform.position;
+        diff.Normalize();
+
+        float rot_z = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rot_z - 90);
+
+        // Move Forwards
+        rigidbody2D.velocity *= 0;
+        rigidbody2D.AddForce(transform.up * Speed * Time.fixedDeltaTime * 10);
+
+    }
+}
